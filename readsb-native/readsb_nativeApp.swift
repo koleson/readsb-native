2fb373542a87d494f77f1e3c3ae5bf660a18ceba
Kiel Oleson <kiel@jusquici.org>
2023-04-14 10:50:09 -0700

    Initial Commit
    add readsb protobuf `.proto`, temporary static `aircraft.pb` for client and for tests, generate protobuf swift from `.proto`
    
    fix protobuf swift generation; add test data; first display in UI
    
    redact distance data
    
    gitignore user data
    
    re-add redacted distance files

---
+++ readsb-native/readsb_nativeApp.swift
@@ -0,0 +1,37 @@
+//
+//  readsb_nativeApp.swift
+//  readsb-native
+//
+//  Created by Kiel Oleson on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct readsb_nativeApp: App {
+    static let testData: Data = {
+        let url = Bundle.main.url(forResource: "aircraft-no-distance", withExtension: "pb")!
+        do {
+            let data = try Data(contentsOf: url)
+            return data
+        } catch {
+            fatalError("couldn't get demo data: \(error.localizedDescription)")
+        }
+    }()
+    
+    static let aircraftsUpdate: AircraftsUpdate = {
+        do {
+            let meta = try AircraftsUpdate(serializedData: testData)
+            return meta
+        } catch {
+            fatalError("couldn't materialize aircraft meta?")
+        }
+        
+    }()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView(update: readsb_nativeApp.aircraftsUpdate)
+        }
+    }
+}
