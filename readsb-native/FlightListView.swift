7a9cd5e66bf8f1cac1bb094046467fc7e52ec0ed
Kiel Oleson <kiel@jusquici.org>
2023-04-14 19:44:04 -0700

    minor flight detail view tweaks

--- readsb-native/FlightListView.swift
+++ readsb-native/FlightListView.swift
@@ -33,20 +33,25 @@ struct FlightDetailView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("\(aircraft.flight)").font(.largeTitle).multilineTextAlignment(.leading)
+            HStack {
+                Text("\(aircraft.flight)").font(.largeTitle).multilineTextAlignment(.leading)
+                Spacer()
+                Text(String(format: "%x", aircraft.addr)).monospaced()
+            }
+            
             Spacer().frame(height: 4.0)
             // squawk field needs work / conversion from octal - kmo 14 apr 2023 15h25
-            //Text("Squawk: \(String(format: "%o", aircraft.squawk))")      // TODO:  octal digits, convert
-            Text("Category: \(aircraft.category)")  // hex?
+            // Text("Squawk: \(String(format: "%o", aircraft.squawk))")      // TODO:  octal digits, convert
+            // Text("Category: \(aircraft.category)")  // hex?
             Text("Alt (Baro): \(aircraft.altBaro) feet")
             Text("Heading (Mag): \(aircraft.magHeading)°")
             Text("IAS: \(aircraft.ias)")
             Text("Lat: \(aircraft.lat)")
             Text("Lon: \(aircraft.lon)")
             Text(verbatim: String(format: "Messages: %d", aircraft.messages))
-//                    Text(verbatim: String(format: "Last Seen: %d ms ago", aircraft.seen))
+//            Text(verbatim: String(format: "Last Seen: %d ms ago", aircraft.seen))
             Spacer()
-        }
+        }.padding(4.0)
     }
 }
 
