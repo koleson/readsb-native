b85a1c797a00998ca2a320c9432793b68d074529
Kiel Oleson <kiel@kmo.email>
2023-06-08 20:14:12 -0700

    import airframe database on launch
    reset every time for now

--- readsb-native/ReADSBNativeApp.swift
+++ readsb-native/ReADSBNativeApp.swift
@@ -10,6 +10,8 @@ import OSLog
 
 @main
 struct ReADSBNativeApp: App {
+    let resetPersistentStoreEveryLaunch = true
+
     var body: some Scene {
         WindowGroup {
             ContentView(viewModel: RootViewModel(update: GlobalPreviewData.aircraftsUpdate)).onAppear {
@@ -28,7 +30,54 @@ struct ReADSBNativeApp: App {
                             }
                             return container
                         }()
-//                    let testAirframe = persistentContainer.m
+
+                    if resetPersistentStoreEveryLaunch {
+                        // based on https://stackoverflow.com/questions/53946485/core-data-how-to-reset-everything-when-setup-with-loadpersistentstores
+                        if let currentStore = persistentContainer.persistentStoreCoordinator.persistentStores.last,
+                           let storeURL = currentStore.url {
+                            do {
+                                try persistentContainer.persistentStoreCoordinator
+                                    .destroyPersistentStore(at: storeURL, type: .sqlite)
+                                try _ = persistentContainer.persistentStoreCoordinator
+                                    .addPersistentStore(type: .sqlite, at: storeURL)
+                            } catch {
+                                persistentContainerLogger.error(
+                                    "error resetting persistent store: \(error.localizedDescription)")
+                            }
+                            persistentContainerLogger.notice("reset persistent store successfully!")
+                        } else {
+                            persistentContainerLogger.error("could not get current store URL to reset the store")
+                        }
+                    }
+
+                    let importContext = persistentContainer.newBackgroundContext()
+
+                    let testFetchRequest = Airframe.fetchRequest()
+                    let context = persistentContainer.viewContext
+                    let preImportObjects = try context.fetch(testFetchRequest)
+                    persistentContainerLogger.notice("pre-import Airframes count: \(preImportObjects.count)")
+
+                    let aircraftDBURL = Bundle.main.url(forResource: "aircraft-database-complete-2023-06",
+                                                        withExtension: "csv")!
+
+                    let importStartTime = CFAbsoluteTimeGetCurrent()
+                    DispatchQueue.global(qos: .background).async {
+                        persistentContainerLogger.notice("starting aircraft import")
+                        let importer = TabularDataAirframeCSVImporter()
+                        importer.importAirframes(fromCSVatURL: aircraftDBURL, intoContext: importContext)
+                        let importEndTime = CFAbsoluteTimeGetCurrent()
+                        let importElapsedTime = importEndTime - importStartTime
+                        persistentContainerLogger.notice("imported airframes DB in \(importElapsedTime) seconds")
+
+                        DispatchQueue.main.async {
+                            do {
+                                let postImportObjects = try context.fetch(testFetchRequest)
+                                persistentContainerLogger.notice("Airframes count: \(postImportObjects.count)")
+                            } catch {
+                                persistentContainerLogger.error("couldn't get post-import objects to get airframes count")
+                            }
+                        }
+                    }
                 }
             }
         }
